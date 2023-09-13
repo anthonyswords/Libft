c6c22ab62e6556728797dbaae8cc3bf77c43c6f3
anthonyswords <toniespadasnavarro@gmail.com>
2023-09-13 14:31:47 +0200

    test isalpha introduced

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: aespadas <aespadas@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 13:31:39 by aespadas          #+#    #+#             */
-/*   Updated: 2023/09/13 13:31:56 by aespadas         ###   ########.fr       */
+/*   Updated: 2023/09/13 14:31:29 by aespadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,29 @@ int	ft_isalpha(int c)
 		return (1);
 	return (0);
 }
+/*
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+
+int main() {
+    // Pruebas con caracteres alfabéticos mayúsculos
+    for (int i = 'A'; i <= 'Z'; i++) {
+        assert(ft_isalpha(i) == isalpha(i));
+    }
+
+    // Pruebas con caracteres alfabéticos minúsculos
+    for (int i = 'a'; i <= 'z'; i++) {
+        assert(ft_isalpha(i) == isalpha(i));
+    }
+
+    // Pruebas con caracteres no alfabéticos
+    assert(ft_isalpha('@') == isalpha('@'));
+    assert(ft_isalpha('9') == isalpha('9'));
+    assert(ft_isalpha(' ') == isalpha(' '));
+    assert(ft_isalpha('\t') == isalpha('\t'));
+
+    printf("Todas las pruebas pasaron con éxito.\n");
+
+    return 0;
+}*/
