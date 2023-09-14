b79840f9f52e88f365464d1c214f1415033dda3b
anthonyswords <toniespadasnavarro@gmail.com>
2023-09-14 10:58:38 +0200

    test done and success

--- ft_atoi.c
+++ ft_atoi.c
@@ -37,7 +37,7 @@ int	ft_atoi(const char *str)
 	}
 	return (result * sign);
 }
-/*
+
 #include <stdio.h>
 #include <assert.h>
 
@@ -67,7 +67,7 @@ int main() {
     // Caso de prueba con cadena vacía
     assert(ft_atoi("") == 0);
 
-    printf("Todas las pruebas pasaron con éxito.\n");
+    printf("Each tests run succesfully.\n");
 
     return 0;
-}*/
+}
