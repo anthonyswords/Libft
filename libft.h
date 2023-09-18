b0f0e3df67e5cdbb528f0a4f3a8bc1fb1babe24b
anthonyswords <toniespadasnavarro@gmail.com>
2023-09-18 17:49:29 +0200

    update

--- libft.h
+++ libft.h
@@ -19,11 +19,14 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *s);	
+
 
 #endif
